a59a86b5029af0012259c3c90648e39d8773f0bc
GregRR <greg.r.roe@gmail.com>
2015-09-01 16:03:18 -0700

    Solutions for Marklar and Fibonacci.

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,9 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    str.gsub!(/\b[a-z]\w{4,}?\b/, "marklar")
+		str.gsub!(/\b[A-Z]\w{4,}?\b/, "Marklar")
+    return str
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -12,7 +14,19 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+  	if nth < 3
+  		return 0
+  	else
+			fib_seq = [1, 1] #start of meaningful sequence
+			nth = nth - 2 #account for defaults already there
+			nth.times do
+				fib_seq << fib_seq[-2] + fib_seq[-1]
+			end
+		end
+		
+		#delete the odd numbers and return sum
+		fib_seq.delete_if {|n| n.odd? }
+		sum = fib_seq.inject(:+)
+		return sum
   end
-
 end
